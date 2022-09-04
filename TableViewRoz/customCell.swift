abad73171b7005e70764cb18d5453a5b49233f2e
mac book <roozh1009@gmail.com>
2022-09-04 10:36:13 +0300

    “rooz”

---
+++ TableViewRoz/customCell.swift
@@ -0,0 +1,44 @@
+//
+//  TableViewCell.swift
+//  TableViewRoz
+//
+//  Created by Ahmed Salah on 04/09/2022.
+//
+
+import UIKit
+
+class customCell: UITableViewCell {
+
+    
+    
+    // tome for salah
+    
+    @IBOutlet weak var fjr: UILabel!
+    @IBOutlet weak var dhr: UILabel!
+    @IBOutlet weak var asr: UILabel!
+    @IBOutlet weak var mgrb: UILabel!
+    @IBOutlet weak var eshaa: UILabel!
+    
+    
+    // name of salah
+    
+    @IBOutlet weak var fjr1: UILabel!
+    @IBOutlet weak var dhr1: UILabel!
+    @IBOutlet weak var asr1: UILabel!
+    @IBOutlet weak var mgrb1: UILabel!
+    @IBOutlet weak var eshaa1: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    
+    
+
+}
